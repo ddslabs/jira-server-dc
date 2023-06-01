e36d2f7430b4202f49bb987653c2437a4ac00a1d
Dmitrij P <ddslabs@gmail.com>
2023-06-01 08:29:12 +0200

    Create execution-time-counter.groovy
    Simple script execution time counter snippet

---
+++ groovy/execution-time-counter.groovy
@@ -0,0 +1,11 @@
+import groovy.time.TimeCategory 
+import groovy.time.TimeDuration
+
+def execStart = new Date()
+/*
+ main script execution
+*/
+def execEnd = new Date()
+ 
+TimeDuration execTotal = TimeCategory.minus(execEnd, execStart)
+println execTotal // or log.gebug("Total execution time $execTotal")
