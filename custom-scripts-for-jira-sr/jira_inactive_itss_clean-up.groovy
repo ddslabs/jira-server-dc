f72d0382189ec4342b3bd438e293696e9aa8dccc
ddslabs <ddslabs@gmail.com>
2023-06-23 10:46:01 +0200

    Inactive ITSSs Clean-up in Jira/JSM

---
+++ custom-scripts-for-jira-sr/jira_inactive_itss_clean-up.groovy
@@ -0,0 +1,34 @@
+/*
+* Unused/Inactive Issue Type Screen Schemes Clean-up in Jira/JSM 
+* Created by Dmitrij P @ June 2023
+*/
+// Main objects
+import com.atlassian.jira.component.ComponentAccessor
+// Logging
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+def log = Logger.getLogger("ITSS Clean-up")
+log.setLevel(Level.DEBUG)
+log.debug("Issue Type Screen Schemes Clean-up - Begin")
+
+// ITSS managements
+def itssManager = ComponentAccessor.getIssueTypeScreenSchemeManager()
+// all ITSSs
+def allITSS = itssManager.issueTypeScreenSchemes
+
+// inactive ITSS Bulk delete
+for (itss in allITSS) {
+    //log.debug("ITSS ID: ${itss.getId()} & Name ${itss.getName()} - Projects: ${itss.getProjects()}")
+    if (!itss.getProjects()) { // !itss.getProjects() - only inactive ITSSs
+        //log.debug("ITSS ID: ${itss.getId()} & Name ${itss.getName()} - Projects: ${itss.getProjects()}")
+        try {
+            //itssManager.removeIssueTypeScreenScheme(itss) // comment to prevent inactive ITSS delete
+            log.debug("ITSS ID: ${itss.getId()} & Name ${itss.getName()} - was REMOVED/DELETED")
+        } catch (Exception e) {
+            log.error(e.getMessage())
+        }
+    }
+}
+// inactive Screens & Screen Schemes are not treated by this scripts (for now)
+log.debug("Issue Type Screen Schemes Clean-up - End")
