b33aab128ca7162f67ba3f43945b0e4bbfde6552
ddslabs <ddslabs@gmail.com>
2023-08-23 14:08:58 +0200

    jira script snippets

--- custom-scripts-for-jira-sr/z_jira_script_snippets.groovy
+++ custom-scripts-for-jira-sr/z_jira_script_snippets.groovy
@@ -13,15 +13,32 @@ Issues.create('ProjKey', 'Story') {
 }
 
 
-log.debug('<<< Project Permission scheme: >>>')
+log.debug('<<< All Project Permission scheme: >>>')
 def allPS = permissionSchemeManager.getSchemes()
 allPS.forEach() { permSchm ->
     log.debug(permSchm.id + ' - ' + permSchm.name)
 }
 
-log.debug('<<< Project Categories: >>>')
+log.debug('<<< All Project Categories: >>>')
 def allCats = projectManager.getAllProjectCategories()
 allCats.forEach() { category ->
     log.debug(category.getId() + ' - ' + category.getName())
 }
 
+log.debug('<<< All Notificaiton Schemes: >>>')
+def allNotifSchms = notificationShcemeManager.getSchemes()
+allNotifSchms.forEach() { notifSchm ->
+    log.debug(notifSchm.id + ' - ' + notifSchm.name)
+}
+
+log.debug('<<< Inactive Notificaiton Schemes: >>>')
+def inactNotSchms = notificationShcemeManager.getUnassociatedSchemes()
+inactNotSchms.forEach() { inNSchm ->
+    log.debug(inNSchm.getId() + ' - ' + inNSchm.getName())
+}
+
+log.debug("<<< All Projects: >>>")
+def allProjs = projectManager.getProjects()
+allProjs.forEach() { p ->
+    log.debug("Project Id <${p.getId()}> - Key <${p.getKey()}> - Name ${p.getName()}")
+}
